bf96076a8f016f9117261e77247312e7fc5065dd
OksanaKozhukh <oksana.kozhukh71090@gmail.com>
2020-03-22 13:22:47 +0200

    card style

--- app/components/animal/AnimalView.js
+++ app/components/animal/AnimalView.js
@@ -15,22 +15,23 @@ export class AnimalView {
     
     getAnimal(el) {
         const card = document.createElement("div");
-        card.classList.add("card", "col", "mb-4");
+        card.classList.add("card-deck", "col", "mb-4");
         
         card.innerHTML = `
-                <div class="card-deck">
-                     <img src="${el.image}" alt="Photo" class="center">
-                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">${el.breed}</li>
-                        <li class="list-group-item">${el.species} ${el.gender}</li>
-                        <li class="list-group-item">${this.convertedDOB(el)}</li>
-                        <li class="list-group-item">Price: ${el.price} UAH</li>
-                    </ul>
-                </div>
-                <div class="card-body">
-                    <button href="#" class="btn btn-outline-secondary">Add to cart</button>
-                    <button href="#" class="btn btn-primary details-button" data-id="${el.id}">&#128062;Details</button>
-                </div>`      
+            <div class="card">
+                <img src="${el.image}" alt="Photo" class="center">
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">${el.breed}</li>
+                            <li class="list-group-item">${el.species} ${el.gender}</li>
+                            <li class="list-group-item">${this.convertedDOB(el)}</li>
+                            <li class="list-group-item">Price: ${el.price} UAH</li>
+                        </ul>
+                        <button type="button" class="btn btn-outline-secondary">Add to cart</button>
+                        <button type="button" class="btn btn-outline-success details-button" data-id="${el.id}">&#128062;Details</button>
+                    </div>
+            </div>` 
+                 
         card.querySelector(".details-button").addEventListener("click", ev => {
             ev.preventDefault();
             this.clickListener(el.id);
